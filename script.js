17c852f82eeac81e0a57d6d4b91fae87502c7457
Yotam <47275263+YotamZiv298@users.noreply.github.com>
2021-06-28 21:36:11 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,34 @@
+const NUM_OF_COL_TASK = 5;
+const NUM_OF_COL_SUB_TASK = 4;
+
+var numOfSubTasks = [];
+
 var taskNum = 1;
-var subTaskNum = 1;
+
+function checkInputValEmpty(elementId) {
+    return document.getElementById(elementId).value == "" ? true : false;
+}
+
+function insertStringToElementValue(elementId, str) {
+    document.getElementById(elementId).value = str;
+}
 
 function addTask() {
     let inputTaskValue = document.getElementById("inputTask").value;
-    if (inputTaskValue == "") {
+
+    // Check if task text input is empty
+    if (checkInputValEmpty("inputTask")) {
         alert("You must write something");
         return;
     }
-    document.getElementById("inputTask").value = "";
+    insertStringToElementValue("inputTask", "");
 
     let body = document.getElementsByTagName("body")[0];
 
     if (taskNum == 1) {
         var tbl = document.createElement("table");
-        tbl.setAttribute("id", "tasksTable")
-        //tbl.setAttribute("border", "1");
+
+        tbl.setAttribute("id", "tasksTable");
         tbl.setAttribute("cellpadding", "10");
 
         body.appendChild(tbl);
@@ -24,33 +38,39 @@ function addTask() {
 
     let tr = document.createElement("tr");
 
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; i < NUM_OF_COL_TASK; i++) {
         let td = document.createElement("td");
-        tr.appendChild(td)
+        tr.appendChild(td);
     }
 
+    // Create complete task checkbox
     let checkBoxCompleteTask = document.createElement("input");
     checkBoxCompleteTask.setAttribute("type", "checkbox");
     checkBoxCompleteTask.setAttribute("onchange", "completeTask(this.className)");
 
+    // Create task input area
     let inputTaskTxt = document.createElement("input");
     inputTaskTxt.setAttribute("readonly", true);
     inputTaskTxt.setAttribute("type", "text");
     inputTaskTxt.setAttribute("value", inputTaskValue);
 
+    // Create edit button
     let buttonEditTask = document.createElement("button");
     buttonEditTask.textContent = "Edit";
     buttonEditTask.setAttribute("onclick", "editTask(this.className)");
 
+    // Create subtask input area
     let inputSubTaskTxt = document.createElement("input");
-    inputSubTaskTxt.setAttribute("id", "inputSubTask" + subTaskNum);
+    inputSubTaskTxt.setAttribute("id", "inputSubTask" + taskNum);
     inputSubTaskTxt.setAttribute("type", "text");
     inputSubTaskTxt.setAttribute("placeholder", "Add subtask...");
 
+    // Create subtask adding button
     let buttonAddSubTask = document.createElement("button");
     buttonAddSubTask.textContent = "Add Sub Task";
-    buttonAddSubTask.setAttribute("onclick", "addSubTask(this.className)");
+    buttonAddSubTask.setAttribute("onclick", "addSubTask(this.closest('td'), this.className)");
 
+    // Create remove task button
     let buttonRemoveTask = document.createElement("button");
     buttonRemoveTask.textContent = "Remove";
     buttonRemoveTask.setAttribute("onclick", "removeTask(this.className)");
@@ -64,7 +84,8 @@ function addTask() {
 
     tr.cells[4].appendChild(buttonRemoveTask);
 
-    for (let i = 0; i < 5; i++) {
+    // Setting class to all elements in task's row
+    for (let i = 0; i < NUM_OF_COL_TASK; i++) {
         for (let j = 0; j < tr.cells[i].children.length; j++) {
             tr.cells[i].children[j].setAttribute("class", "task" + taskNum);
         }
@@ -75,9 +96,9 @@ function addTask() {
 }
 
 function completeTask(className) {
-    let task = document.getElementsByClassName(className);
-    let chckbxTaskComplete = task[0];
-    let inputTask = task[1];
+    let row = document.getElementsByClassName(className);
+    let chckbxTaskComplete = row[0];
+    let inputTask = row[1];
 
     if (chckbxTaskComplete.checked) {
         inputTask.setAttribute("style", "text-decoration: line-through;");
@@ -87,9 +108,9 @@ function completeTask(className) {
 }
 
 function editTask(className) {
-    let task = document.getElementsByClassName(className);
-    let inputTask = task[1];
-    let buttonEditTask = task[2];
+    let row = document.getElementsByClassName(className);
+    let inputTask = row[1];
+    let buttonEditTask = row[2];
 
     if (buttonEditTask.innerHTML == "Edit") {
         inputTask.removeAttribute("readonly");
@@ -100,13 +121,79 @@ function editTask(className) {
     }
 }
 
-function addSubTask(className) {
-    let inputSubTaskValue = document.getElementById("inputSubTask" + subTaskNum).value;
+function addSubTask(tdParent, className) {
+    let inputSubTaskValue = document.getElementById("inputSubTask" + className.slice(-1)).value;
+
+    // Check if subtask text input is empty
+    if (checkInputValEmpty("inputSubTask" + className.slice(-1))) {
+        alert("You must write something");
+        return;
+    }
+    insertStringToElementValue("inputSubTask" + className.slice(-1), "");
+    if (className.slice(-1) - 1 == numOfSubTasks.length) {
+        numOfSubTasks.push(1);
+    }
+    if (numOfSubTasks[className.slice(-1) - 1] == 1) {
+        var tbl = document.createElement("table");
+
+        tbl.setAttribute("id", "subTasksTable" + className.slice(-1));
+        tbl.setAttribute("cellpadding", "5");
+
+        tdParent.appendChild(tbl);
+    } else {
+        var tbl = document.getElementById("subTasksTable" + className.slice(-1));
+    }
+
+    let tr = document.createElement("tr");
+
+    for (let i = 0; i < NUM_OF_COL_SUB_TASK; i++) {
+        let td = document.createElement("td");
+        tr.appendChild(td);
+    }
+
+    // Create complete subtask checkbox
+    let checkBoxCompleteSubTask = document.createElement("input");
+    checkBoxCompleteSubTask.setAttribute("type", "checkbox");
+    checkBoxCompleteSubTask.setAttribute("onchange", "completeTask(this.className)");
+
+    // Create subtask input area
+    let inputSubTaskTxt = document.createElement("input");
+    inputSubTaskTxt.setAttribute("readonly", true);
+    inputSubTaskTxt.setAttribute("type", "text");
+    inputSubTaskTxt.setAttribute("value", inputSubTaskValue);
+
+    // Create edit button
+    let buttonEditSubTask = document.createElement("button");
+    buttonEditSubTask.textContent = "Edit";
+    buttonEditSubTask.setAttribute("onclick", "editTask(this.className)");
+
+    // Create remove task button
+    let buttonRemoveSubTask = document.createElement("button");
+    buttonRemoveSubTask.textContent = "Remove";
+    buttonRemoveSubTask.setAttribute("onclick", "removeTask(this.className)");
+
+    tr.cells[0].appendChild(checkBoxCompleteSubTask);
+    tr.cells[1].appendChild(inputSubTaskTxt);
+    tr.cells[2].appendChild(buttonEditSubTask);
+    tr.cells[3].appendChild(buttonRemoveSubTask);
+
+    // Setting class to all elements in task's row
+    for (let i = 0; i < NUM_OF_COL_SUB_TASK; i++) {
+        for (let j = 0; j < tr.cells[i].children.length; j++) {
+            tr.cells[i].children[j].setAttribute("class", "subtask" + "_" + className.slice(-1) + "_" + numOfSubTasks[className.slice(-1) - 1]);
+        }
+    }
+
+    tbl.appendChild(tr);
+    numOfSubTasks[className.slice(-1) - 1]++;
+
+
+    /*let inputSubTaskValue = document.getElementById("inputSubTask" + subTaskNum).value;
     if (inputSubTaskValue == "") {
         alert("You must write something!");
         return;
     }
-    document.getElementById("inputSubTask" + subTaskNum).value = "";
+    insertStringToElementValue("inputSubTask" + subTaskNum, "");
 
     let tdParent = document.getElementsByTagName("tr")[subTaskNum][3];
 
@@ -152,7 +239,7 @@ function addSubTask(className) {
 
     tbl.appendChild(tr);
     taskNum++;
-
+    */
 }
 
 function removeTask(className) {
@@ -174,19 +261,19 @@ function search() {
     try {
         var table = document.getElementById("tasksTable");
         var tr = table.getElementsByTagName("tr");
-
-        // Loop through all table rows, and hide those who don't match the search query
-        for (let i = 0; i < tr.length; i++) {
-            td = tr[i].getElementsByTagName("td")[1];
-            if (td) {
-                txtValue = td.children[0].value;
-                if (txtValue.toUpperCase().indexOf(filter) > -1) {
-                    tr[i].style.display = "";
-                } else {
-                    tr[i].style.display = "none";
-                }
+    } catch (error) {
+        return;
+    }
+    // Loop through all table rows, and hide those who don't match the search
+    for (let i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[1];
+        if (td) {
+            txtValue = td.children[0].value;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
             }
         }
-    } catch (error) {
     }
 }
